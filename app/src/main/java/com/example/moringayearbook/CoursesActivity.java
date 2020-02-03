d051286831fd1152a5a8f937ecd9076930613af2
MoringaSchool <you@example.com>
2020-02-03 11:02:34 +0300

    add array adapter in list view

--- app/src/main/java/com/example/moringayearbook/CoursesActivity.java
+++ app/src/main/java/com/example/moringayearbook/CoursesActivity.java
@@ -3,12 +3,31 @@ package com.example.moringayearbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CoursesActivity extends AppCompatActivity {
-
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
+
+        listView = (ListView) findViewById(R.id.ListView);
+
+        final String[] strArr = new String[]{
+                "Shadrack",
+                "Mark",
+                "Mburia",
+                "Justus",
+                "Hamerton",
+                "Philip",
+                "Kamala",
+                "Omonche",
+                "Joel",
+                "Nheru"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,android.R.id.text1, strArr);
     }
 }
