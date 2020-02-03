f760d3a8a87cbefdefac796b6acfeca2ef440526
MoringaSchool <you@example.com>
2020-02-03 11:05:10 +0300

    set adapte to List view

--- app/src/main/java/com/example/moringayearbook/CoursesActivity.java
+++ app/src/main/java/com/example/moringayearbook/CoursesActivity.java
@@ -3,7 +3,9 @@ package com.example.moringayearbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,7 +13,9 @@ public class CoursesActivity extends AppCompatActivity {
     ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_courses);
 
         listView = (ListView) findViewById(R.id.ListView);
@@ -29,5 +33,16 @@ public class CoursesActivity extends AppCompatActivity {
                 "Nheru"
         };
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,android.R.id.text1, strArr);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+
+
+            }
+        });
     }
 }
