aff3f67e7cb81a7fbe8eac4731eef352d91aa52b
MoringaSchool <you@example.com>
2020-02-03 10:25:13 +0300

    add new activity

--- app/src/main/java/com/example/moringayearbook/MainActivity.java
+++ app/src/main/java/com/example/moringayearbook/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.moringayearbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mFindCourseButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        mFindCourseButton = (Button) findViewById(R.id.findCoursesButton);
+        mFindCourseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //                Toast.makeText(MainActivity.this, "HelloWorld!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, CoursesActivity.class);
+                startActivity(intent);
+            }
+        });
+        }
 }
