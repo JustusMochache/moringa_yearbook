802233852001d90d166d433e9776dcc7ebe381af
MoringaSchool <you@example.com>
2020-02-03 16:09:32 +0300

    debug contact activity by removing scroll view to linear layout

--- app/src/main/java/com/example/moringayearbook/CoursesActivity.java
+++ app/src/main/java/com/example/moringayearbook/CoursesActivity.java
@@ -2,6 +2,7 @@ package com.example.moringayearbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
@@ -14,7 +15,17 @@ import android.widget.Toast;
 public class CoursesActivity extends AppCompatActivity {
 
     private Button mFindContactButton;
-    ListView listView;
+    private ListView listView;
+
+    final String[] strArr = new String[]{
+            "Angular",
+            "CSS",
+            "Javascript",
+            "Java",
+            "Android",
+            "Spark",
+            "Firebase"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -22,19 +33,17 @@ public class CoursesActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_courses);
 
-        mFindContactButton = (Button) findViewById(R.id.findCoursesButton);
-        mFindContactButton.setOnClickListener();
+        mFindContactButton = (Button) findViewById(R.id.findContactButton);
+        mFindContactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CoursesActivity.this, ContactActivity.class);
+                startActivity(intent);
+            }
+        });
+
                 listView = (ListView) findViewById(R.id.ListView);
 
-        final String[] strArr = new String[]{
-                "Angular",
-                "CSS",
-                "Javascript",
-                "Java",
-                "Android",
-                "Spark",
-                "Firebase"
-        };
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,android.R.id.text1, strArr);
 
         listView.setAdapter(adapter);
